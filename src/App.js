06157deddeba2d55fd0babcbc6890bf90f0f8dd3
swati-04 <itwa0407@gmail.com>
2021-08-13 17:11:31 +0530

    Testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import Todo from './components/todo'
 function App() {
+  const todos=[
+    {id:1,title:"bring grocery",completed:false},
+    {id:2,title:"wash Dishes",completed:true},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        todos.map((x)=>{
+          return (<Todo todo={x}/>)
+        })
+      }
     </div>
   );
 }
